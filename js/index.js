dec1d32ecafddd2c819257930574ee60404357a4
Alina Pavlyuk <alino4kap12@gmail.com>
2018-10-09 21:29:05 +0000

    Fixed image styles and js

---
+++ js/index.js
@@ -0,0 +1,30 @@
+const overlay = document.querySelector('.overlay');
+const imageList = document.querySelectorAll('img');
+let currentFixedImage;
+imageList.forEach(el => el.addEventListener('click', addFixedClasses));
+overlay.addEventListener('click', removeFixedClasses);
+
+function addFixedClasses(e) {
+    const image = e.target;
+    const imageWidth = image.width;
+    const imageHeight = image.height;
+    
+    overlay.classList.add('overlay-visible');
+    image.classList.add('fixed-image');
+    
+    image.style.marginLeft = (-imageWidth / 2) + 'px';
+    image.style.marginTop = (-imageHeight / 2) + 'px';
+    
+    
+    currentFixedImage = image;
+}
+
+function removeFixedClasses(e) {
+    overlay.classList.remove('overlay-visible');
+    currentFixedImage.classList.remove('fixed-image');
+    currentFixedImage.style.marginLeft = '';
+    currentFixedImage.style.marginTop = '';
+}
+
+
+
